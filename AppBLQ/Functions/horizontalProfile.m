24da802cf99a520ac4a22c394c1ea55aafeef7f3
Usuario <Usuario@DESKTOP-LSENM2D>
2019-06-06 18:13:38 +0200

    New functions for vertical and horizontal profiles

---
+++ AppBLQ/Functions/horizontalProfile.m
@@ -0,0 +1,43 @@
+function horizontalProfile(Info)
+%LineasPromedio = 3;
+if rem(length(Info.DistanciaColumnas),2)
+    LongitudPerfil = length(Info.DistanciaColumnas)+1; %Poner ese +1 sólo si se ha ajustado el (0,0) previamente en QPIStudy.m
+else
+    LongitudPerfil = length(Info.DistanciaColumnas);
+end
+
+Perfiles = zeros(length(Info.Energia),LongitudPerfil);
+%Perfiles2 = zeros(length(Energia),LongitudPerfil);
+
+for k=1:length(Info.Energia)
+    Perfiles(k,:) = Info.Transformadas{k}(:,length(Info.DistanciaColumnas)/2+1);
+    %Perfiles2(k,:) = TransformadasSimetrizadas{k}(:,1+Filas/2);
+    %Perfiles(k,1:LongitudPerfil-1)= diff(Perfiles(k,:));
+end
+
+% mediaTotal = mean(mean(Perfiles));
+% FlattenMatrix=Perfiles;
+% for i = 1:length(Perfiles(:,1))
+%                FlattenMatrix(:,i) = FlattenMatrix(:,i) - (mean(FlattenMatrix(:,i)) - mediaTotal);
+% end
+% PerfilesPromedio = (Perfiles + Perfiles2)/2;
+% PerfilesFlatten=Flatten(Perfiles,[1,1]);
+
+a=figure;
+%surf((ParametroRed/TamanhoReal)*(1:LongitudPerfil-1),Energia,Perfiles(:,1:LongitudPerfil-1))
+imagesc(Info.DistanciaFourierColumnas*2*Info.ParametroRedFilas,Info.Energia,Perfiles);
+axis([0 1 min(InfoStruct.Energia) max(InfoStruct.Energia)]);
+%axis([0 1 -85 85]);
+b=gca;
+b.Colormap = parula;
+b.YDir='normal';
+b.YLabel.String = '\fontsize{15} Energy (meV)';
+b.XLabel.String = '\fontsize{15} k_{x} (\pi/a)';
+b.LineWidth = 2;
+b.FontWeight = 'bold';
+% b.Position = b.OuterPosition;
+%b.CLim=[0 0.15];
+%b.CLim=[min(min(Perfiles)) max(max(Perfiles))];
+%colormap gray
+    
+end
